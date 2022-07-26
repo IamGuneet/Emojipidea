4c3f59106a9622a57f1e19962fa5931fc1811c98
Guneet <guneet.iam@gmail.com>
2022-07-26 19:29:56 +0530

    Updates

---
+++ precache-manifest.0fb1e62eec4ec5bd5091d2aaeaf42e94.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26dedd86377c8efa33f5fd8f972bdb1b",
+    "url": "/Emojipidea/index.html"
+  },
+  {
+    "revision": "323f6d0b4206bcf9ebe4",
+    "url": "/Emojipidea/static/js/2.7fbf7892.chunk.js"
+  },
+  {
+    "revision": "2108c8f3548337671963",
+    "url": "/Emojipidea/static/js/main.61aff4af.chunk.js"
+  },
+  {
+    "revision": "ca76086a02c38b3c3f0a",
+    "url": "/Emojipidea/static/js/runtime-main.44023b4c.js"
+  }
+]);
